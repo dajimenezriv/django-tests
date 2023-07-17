a1739cc66e7861c4cd394920f12e20c8627a9b4b
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2023-07-17 11:43:13 +0200

    Codelens working in each function

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const testCodeLensProvider: vscode.CodeLensProvider = {
+        provideCodeLenses(document: vscode.TextDocument): vscode.ProviderResult<vscode.CodeLens[]> {
+            const codeLenses: vscode.CodeLens[] = [];
+            const classRegex = /class\s+(\w+)\(BaseTestCase\):/g;
+            const testRegex = /def\s+(test_\w+)\(self\):/g;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "docker-tests" is now active!');
+            let classMatch;
+            while ((classMatch = classRegex.exec(document.getText()))) {
+                const className = classMatch[1];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('docker-tests.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Docker Tests!');
-	});
+                let testMatch;
+                while ((testMatch = testRegex.exec(document.getText()))) {
+                    const testName = testMatch[1];
+                    const range = document.lineAt(document.positionAt(testMatch.index).line).range;
+                    const command: vscode.Command = {
+                        title: 'Run Test',
+                        command: 'extension.runTest',
+                        arguments: [className, testName]
+                    };
+                    const codeLens = new vscode.CodeLens(range, command);
+                    codeLenses.push(codeLens);
+                }
+            }
 
-	context.subscriptions.push(disposable);
+            return codeLenses;
+        }
+    };
+
+    context.subscriptions.push(
+        vscode.languages.registerCodeLensProvider('python', testCodeLensProvider),
+        vscode.commands.registerCommand('extension.runTest', (className: string, testName: string) => {
+            const command = `docker compose exec wsgi_backend python manage.py test naranjas_daniel.tests.${className}.${testName}`;
+            vscode.window.terminals.forEach(terminal => {
+                terminal.sendText(command);
+                terminal.show();
+            });
+        })
+    );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
